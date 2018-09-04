c3e34d5f7a0de95c0578397c8182e8272bb3fac5
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-04 03:25:27 +0000

    minor edits

--- download_avatars.js
+++ download_avatars.js
@@ -6,7 +6,6 @@ const mkdirp = require('mkdirp');
 //authentication credentials
 const credentials = {
   username: process.env.GIT_USER,
-  password: process.env.GIT_PASS,
   token: process.env.GIT_TOKEN,
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET
@@ -22,13 +21,12 @@ let destination = path[2] !== undefined ? `./${path[2]}` : './avatars';
 let getURL = `${url}/${path[0]}/${path[1]}`
 
 
-
 function githubAvatarDownloader(url, path){
   //if /avatars doesn't exist, create it (or create new dir based on third arg)
   if(!fs.existsSync(destination)){
     mkdirp(destination, (err) => {
       if(err) console.error(err);
-      else console.log(`Directory created in: ${destination}`);
+      else console.log(`Directory created in: ${destination}.`);
     });
   };
 
@@ -40,6 +38,7 @@ function githubAvatarDownloader(url, path){
   });
 }
 
+//find repo contributors
 function getRepoContributors(url, cb){
   let options = {
     url: `${url}/contributors`,
@@ -54,7 +53,8 @@ function getRepoContributors(url, cb){
   });
 };
 
-function downloadImgByURL(url, userName, filePath, ){
+//download user images
+function downloadImgByURL(url, userName, filePath){
 
   request.get(url)
     .on('error', (err) => {
@@ -76,4 +76,9 @@ function downloadImgByURL(url, userName, filePath, ){
     });
 }
 
-githubAvatarDownloader(getURL, destination);
+//check for passed args
+if(process.argv.slice(2).length < 2){
+  console.error('Please pass an <owner> and <repo> argument as well as an optional <directory> argument. If you don\'t pass a <directory> argument, images will be downloaded to ./avatars');
+}else{
+  githubAvatarDownloader(getURL, destination);
+}
