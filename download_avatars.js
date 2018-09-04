08c67cf27ee8ee7206590935f662900b23166f66
Benji-Leboe <benjamin.r.leboe@gmail.com>
2018-09-04 03:08:16 +0000

    implemented full functionality

--- download_avatars.js
+++ download_avatars.js
@@ -1,8 +1,7 @@
-const dotenv = require('dotenv').config();
+require('dotenv').config();
 const request = require('request');
 const fs = require('fs');
 const mkdirp = require('mkdirp');
-const github = require('octonode');
 
 //authentication credentials
 const credentials = {
@@ -14,43 +13,36 @@ const credentials = {
 }
 
 //define source URL
-const url = 'https://api.github.com';
+const url = 'https://api.github.com/repos';
 //get args from node
 let path = process.argv.slice(2);
 //assign new dir name if passed otherwise default to /avatars
 let destination = path[2] !== undefined ? `./${path[2]}` : './avatars';
+//concat URL path
+let getURL = `${url}/${path[0]}/${path[1]}`
+
+
 
 function githubAvatarDownloader(url, path){
   //if /avatars doesn't exist, create it (or create new dir based on third arg)
-  if(fs.existsSync(destination)){
+  if(!fs.existsSync(destination)){
     mkdirp(destination, (err) => {
       if(err) console.error(err);
       else console.log(`Directory created in: ${destination}`);
     });
-  }
+  };
 
-  let getURL = `${url}/${path[0]}/${path[1]}`
-  
-  request.get(getURL)
-    .on('error', (err) => {
-      console.error(err);
-    })
-    .on('response', (response) => {
-      console.log(`Response code: ${response.statusCode}\nResponse message: ${response.statusMessage}\nContent type: ${response.headers['content-type']}`);
-      if(response.statusCode >= 400){
-        console.log('Download failed.');
-      }else{
-        console.log(`Downloading Avatar image(s) to ${destination}...`);
-      }
-    })
-    .on('finish', () => {
-      console.log('Avatar(s) downloaded!')
-    })
+  getRepoContributors(url, (err, result) => {
+    console.log(`Errors: ${err}`);
+    JSON.parse(result).forEach((item) => {
+      downloadImgByURL(item.avatar_url, item.login, path);
+    });
+  });
 }
 
-function getRepoContributors(repoOwner, repoName, cb){
+function getRepoContributors(url, cb){
   let options = {
-    url: `https://api.github.com/repos/${repoOwner}/${repoName}/contributors`,
+    url: `${url}/contributors`,
     headers: {
       'User-Agent': 'request',
       'Authorization': `token ${credentials.token}`
@@ -62,14 +54,26 @@ function getRepoContributors(repoOwner, repoName, cb){
   });
 };
 
-getRepoContributors("jquery", "jquery", (err, result) => {
-  console.log(`Errors: ${err}`);
-  let results = JSON.parse(result);
-  results.forEach((item) => {
-    console.log(item.avatar_url);
-  });
-});
+function downloadImgByURL(url, userName, filePath, ){
 
+  request.get(url)
+    .on('error', (err) => {
+      console.error(err);
+    })
+    .on('response', (response) => {
+      console.log(`Response code: ${response.statusCode}\nResponse message: ${response.statusMessage}\nContent type: ${response.headers['content-type']}`);
+      if(response.statusCode >= 400){
+        console.log('Download failed.');
+      }else{
+        console.log(`Downloading ${userName}'s Avatar image to ${filePath}...`);
+      }
+    })
+    .pipe(fs.createWriteStream(`${filePath}/${userName}.jpg`).on('error', (err) => {
+      console.error(err);
+    }))
+    .on('finish', () => {
+      console.log(`${userName}'s Avatar downloaded!`)
+    });
+}
 
-
-// githubAvatarDownloader(url, path);
+githubAvatarDownloader(getURL, destination);
